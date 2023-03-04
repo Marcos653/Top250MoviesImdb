d603350cd6f83162412611c76ad208a41c3107cc
Marcos653 <viniciustatuta@gmail.com>
2023-03-04 11:05:26 -0300

    feat: implement get api imdb

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,21 @@
 package org.example;
 
+import java.io.IOException;
+import java.net.*;
+import java.net.http.*;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final var key = System.getenv("SECRET_KEY");
+
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest
+                .newBuilder()
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/" + key))
+                .GET()
+                .build();
+        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
     }
 }
